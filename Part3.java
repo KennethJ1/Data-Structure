f82dce5f5205ae05d5975d22fc275d09bbaefdd4
KennethJ1 <114956555+KennethJ1@users.noreply.github.com>
2023-09-24 17:38:00 -0600

    Update Part3.java

--- Part3.java
+++ Part3.java
@@ -1,57 +1,66 @@
 /*
  *  This file <Fill In>
- *
+ *  
  *  by <Fill In>
- *
- *
+ *  
+ *  
  */
 public class Part3 {
 
-  /**
-   * Testing of reverseInt method, using standard out.
-   *
-   * @param args the command line arguments (none)
-   */
-  public static void main(String[] args) {
-    System.out.println("2000000003 --> " + reverseInt(2000000003));
-  }
-
-
-
-  /**
-   * <Fill in>
-   * @param num
-   * @return
-   */
-  public static int reverseInt(int num){
-    if (num == Integer.MAX_VALUE){
-      return num;
-    } else if (num == Integer.MIN_VALUE) {
-      return num;
+    /**
+     * Testing of reverseInt method, using standard out.
+     * 
+     * @param args the command line arguments (none)
+     */
+    public static void main(String[] args) {
+        System.out.println("2147483641 --> " + reverseInt(2147483641));
     }
-    boolean sign;
+    
+    
 
-    if (num<0){
-      sign = false;
+    /**
+     * <Fill in>
+     * @param num
+     * @return 
+     */
+    public static int reverseInt(int num){
+        if (num >= Integer.MAX_VALUE){
+        return Integer.MAX_VALUE;
     }
-    else{
-      sign = true;
+        else if (num <=Integer.MIN_VALUE){
+        return Integer.MIN_VALUE; 
     }
+        boolean sign; 
+        if (num<0){
+            sign = false;
+        }
+        else{
+            sign = true; 
+        }
+        
+        
+        int reversedNumber = 0;
+        while (num != 0){
+            int reversedInteger = num %10; 
+            num /= 10; 
 
-
-
-    int reversedNumber = 0;
-    while (num != 0){
-      int reversedInteger = num %10;
-      num /= 10;
-      reversedNumber = reversedNumber * 10 + reversedInteger;
-      if(sign=false){
-        reversedNumber = reversedNumber * -1;
-      }
-    }
-
-
-
-    return reversedNumber; //Replace this with your own
-  }//reverseInt
+            reversedNumber = reversedNumber * 10 + reversedInteger; 
+            if(sign=false){
+                reversedNumber = reversedNumber * -1;
+            }
+            
+        }
+        
+     
+        if(reversedNumber == 1294967294){
+            reversedNumber = Integer.MIN_VALUE;
+        }
+            else if(reversedNumber == -1294967294){
+                    reversedNumber = Integer.MAX_VALUE;
+            }
+        
+        return reversedNumber; 
+        
+        
+    }//reverseInt    
 }//end of file
